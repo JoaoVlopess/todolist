86b9891592a9f5ba0c71aae0398a6bf32ddea16e
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2024-11-01 14:47:11 -0300

    completed the implementation of functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,79 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useState } from "react";
+import { TodoItem } from "./types/TodoItem";
+
+const Page = () => {
+  const [itemInput, setItemInput] = useState('');
+  const [list, setList] = useState<TodoItem[]>([
+    {label:"Lopes", checked: false}
+  ]);
+
+  const handleAddButton = () => {
+    if (itemInput.trim() !== ''){
+      setList([...list,{label:itemInput, checked: false}]);
+      setItemInput('');
+    }
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleAddButton();
+    }
+  }
+
+  const deleteItem = (index: number) => {
+    setList(list.filter((item, key) => key !== index));
+  }
+
+  const toggleItem = (index: number) => {
+    let newLIst = [...list]
+    newLIst[index].checked = !newLIst[index].checked
+    setList(newLIst);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full h-screen bg-slate-800 flex  justify-center p-6">
+      <div className="w-full max-w-xl my-7 border-2 rounded-3xl  text-black shadow shadow-black bg-white">
+        
+        <div className="p-5 font-bold text-2xl  flex items-center justify-center">TO DO LIST</div>
+        <div className="px-20 pb-7 font text-xl border-b flex ">
+          <input 
+          type="text" 
+          placeholder="O que deseja fazer?"
+          className="p-3 w-full h-full border-2 border-black rounded-full mr-7 bg-Inputcolor"
+          value = {itemInput}
+          onChange={e => setItemInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <button 
+          onClick={handleAddButton} 
+          className="border text-white border-black bg-purple-500 rounded-full w-12 h-10 flex items-center justify-center justify-self-center">+
+          </button>
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <ul className="w-full px-5 text-xl">
+
+        {list.map((item,index)=> (
+          <li key={index} className="mx-8 my-3 p-2 flex items-center">
+           <input
+            type="checkbox"
+            className="form-checkbox appearance-none border border-gray-400 h-6 w-6 mr-4 rounded-full shadow-md checked:bg-purple-800 focus:outline-none cursor-pointer  transition-colors duration-300"
+            checked ={item.checked}
+            onClick={() => toggleItem(index)}
+            />
+            <p 
+            className= {`${item.checked === true ? "text-gray-400 line-through" : "text-black" }`}>{item.label}</p> 
+            <button onClick={() => deleteItem(index)} className="ml-auto"> <img src="assets/close.png" alt="" className="h-4 w-4" /> </button>
+          </li>
+        ))}
+
+        </ul>
+
+      </div>
     </div>
   );
 }
+
+export default Page;
